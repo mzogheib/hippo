091bb6a2c1db5ee706d04bb2e10717b9132fdb57
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-10-29 10:23:54 +1100

    Navigate to home on root

--- apps/spa/src/AppLayout.tsx
+++ apps/spa/src/AppLayout.tsx
@@ -1,9 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import AppHeading from "./components/AppHeading";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 function AppLayout(): JSX.Element {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      navigate("/home");
+    }
+  }, [pathname]);
+
   return (
     <div>
       <AppHeading />
