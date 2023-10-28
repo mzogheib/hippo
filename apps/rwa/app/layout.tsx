0a09ec3bc82cd9b8ef4c2a5302b07f2e9490485b
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-10-29 08:08:21 +1100

    Revert "Install MUI"
    This reverts commit 0bab2f250bb5c630ead6aea06f9792f257d3e3ee.

--- apps/rwa/app/layout.tsx
+++ apps/rwa/app/layout.tsx
@@ -1,8 +1,4 @@
 import "./globals.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import type { PropsWithChildren } from "react";
 
 export default function RootLayout({
