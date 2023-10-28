588e111d6e8d57338364b214404d7193ed4ef077
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-10-29 08:20:48 +1100

    Add NavBar

--- apps/rwa/app/layout.tsx
+++ apps/rwa/app/layout.tsx
@@ -1,12 +1,16 @@
-import "./globals.css";
 import type { PropsWithChildren } from "react";
+import NavBar from "./components/NavBar";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: PropsWithChildren): JSX.Element {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
