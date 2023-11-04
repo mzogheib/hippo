39340d14e74e50613fe90c3a6e522ae86b72a76c
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-11-05 07:50:25 +1100

    Add signup route and prompt

--- apps/rwa/app/api/auth/[auth0]/route.ts
+++ apps/rwa/app/api/auth/[auth0]/route.ts
@@ -1,5 +1,7 @@
 import type { HandleAuth } from "@auth0/nextjs-auth0";
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 // For some reason the inferred type of handleAuth is any
-export const GET = handleAuth() as HandleAuth;
+export const GET = handleAuth({
+  signup: handleLogin({ authorizationParams: { screen_hint: "signup" } }),
+}) as HandleAuth;
--- apps/rwa/app/components/AuthBar/index.tsx/index.tsx
+++ apps/rwa/app/components/AuthBar/index.tsx/index.tsx
@@ -2,16 +2,29 @@
 
 import { useUser } from "@auth0/nextjs-auth0/client";
 
+function AuthenticatedContent(): JSX.Element {
+  return <a href="/api/auth/logout">Logout</a>;
+}
+
+function UnauthenticatedContent(): JSX.Element {
+  return (
+    <>
+      <a href="/api/auth/login">Login</a>
+      &nbsp;or&nbsp;
+      <a href="/api/auth/signup">Sign up</a>
+    </>
+  );
+}
+
 function AuthBar(): JSX.Element {
   const { user, isLoading } = useUser();
 
-  const label = user ? "Logout" : "Login";
-  const href = user ? "api/auth/logout" : "api/auth/login";
+  const content = user ? <AuthenticatedContent /> : <UnauthenticatedContent />;
 
   return (
     <nav>
       {isLoading ? <span>Loading...</span> : null}
-      {!isLoading ? <a href={href}>{label}</a> : null}
+      {!isLoading ? content : null}
     </nav>
   );
 }
