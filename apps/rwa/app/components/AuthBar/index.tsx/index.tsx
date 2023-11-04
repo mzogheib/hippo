97f70b7361ac6aed8e5e0a20d8c9f30371fcc497
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-11-05 07:29:47 +1100

    Add loading placeholder

--- apps/rwa/app/components/AuthBar/index.tsx/index.tsx
+++ apps/rwa/app/components/AuthBar/index.tsx/index.tsx
@@ -3,14 +3,15 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 function AuthBar(): JSX.Element {
-  const { user } = useUser();
+  const { user, isLoading } = useUser();
 
   const label = user ? "Logout" : "Login";
   const href = user ? "api/auth/logout" : "api/auth/login";
 
   return (
     <nav>
-      <a href={href}>{label}</a>
+      {isLoading ? <span>Loading...</span> : null}
+      {!isLoading ? <a href={href}>{label}</a> : null}
     </nav>
   );
 }
