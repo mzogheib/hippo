f7dc99c1ae760c21d7f71111f478038e634a3fb7
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-11-05 07:34:30 +1100

    Protect profile route

--- apps/rwa/app/api/profile/route.ts
+++ apps/rwa/app/api/profile/route.ts
@@ -1,12 +1,15 @@
+import { withApiAuthRequired } from "@auth0/nextjs-auth0";
 import type { Profile } from "profile-service";
 import { NextResponse } from "next/server";
 import { getProfile as getProfile_ } from "profile-service";
 
-export const GET = async (): Promise<NextResponse<Profile>> => {
-  const data = await getProfile_();
+export const GET = withApiAuthRequired(
+  async (): Promise<NextResponse<Profile>> => {
+    const data = await getProfile_();
 
-  return NextResponse.json(data);
-};
+    return NextResponse.json(data);
+  },
+);
 
 export const getProfile = async (): Promise<Profile> => {
   const response = await fetch("http://localhost:3000/api/profile");
