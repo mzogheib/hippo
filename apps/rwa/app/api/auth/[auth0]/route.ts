3fceab705c1336aa0d9b3e03d276afbf6344d101
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-11-06 09:56:31 +1100

    Add audience and scope to login handler

--- apps/rwa/app/api/auth/[auth0]/route.ts
+++ apps/rwa/app/api/auth/[auth0]/route.ts
@@ -3,5 +3,11 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 // For some reason the inferred type of handleAuth is any
 export const GET = handleAuth({
+  login: handleLogin({
+    authorizationParams: {
+      audience: "http://localhost:4000",
+      scope: "openid read:profile",
+    },
+  }),
   signup: handleLogin({ authorizationParams: { screen_hint: "signup" } }),
 }) as HandleAuth;
