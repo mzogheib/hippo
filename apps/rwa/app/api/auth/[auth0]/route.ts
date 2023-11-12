949ee1ec7735806877c929b9d099e5559b00f5ad
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-11-13 07:54:47 +1100

    Use new scopes variable

--- apps/rwa/app/api/auth/[auth0]/route.ts
+++ apps/rwa/app/api/auth/[auth0]/route.ts
@@ -1,12 +1,11 @@
 import type { HandleAuth } from "@auth0/nextjs-auth0";
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { scopes } from "profile-service-sdk";
+
+const scope = `openid profile offline_access ${scopes.readAllData}`;
 
 // For some reason the inferred type of handleAuth is any so casting to HandleAuth
 export const GET = handleAuth({
-  login: handleLogin({
-    authorizationParams: {
-      scope: "openid profile read:all_data offline_access",
-    },
-  }),
+  login: handleLogin({ authorizationParams: { scope } }),
   signup: handleLogin({ authorizationParams: { screen_hint: "signup" } }),
 }) as HandleAuth;
